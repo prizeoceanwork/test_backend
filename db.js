64252a7253a499b84670023c366d1f7d0823e1d1
prizeoceanwork <prizeoceanwork@gmail.com>
2025-09-08 17:59:33 +0500

    .env updated

--- db.js
+++ db.js
@@ -6,10 +6,10 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+  connectionString: process.env.DATABASE_URL,
 });
 
-if (!process.env.POSTGRES_URL) {
+if (!process.env.DATABASE_URL) {
   throw new Error("POSTGRES_URL is not set in environment variables");
 }else {
   console.log("POSTGRES_URL is set");
